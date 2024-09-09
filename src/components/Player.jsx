42448562b42ac3399c780fec0f7a0ac5c66ff6be
Mark Fasel <markfasel@gmail.com>
2024-09-09 09:13:17 -0400

    using state as example

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [entereredPlayerName, setEnteredPlayerName] = useState(null)
+    const [submitted, setSubmitted] = useState(false)
+    function handleChange(event) {
+        setSubmitted(false)
+        setEnteredPlayerName(event.target.value)
+    }
+
+    function handleClick() {
+        setSubmitted(true)
+    }
+
+    return (
+        <section id="player">
+            <h2>
+                Welcome {submitted ? entereredPlayerName : 'unknown entity'}
+            </h2>
+            <p>
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={entereredPlayerName}
+                />
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    )
 }
