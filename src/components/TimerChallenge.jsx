94d56bacf7decc2b1f39dfaa3ae8c7956de32f6f
Mark Fasel <markfasel@gmail.com>
2024-09-10 12:45:35 -0400

    update with onreset prop ref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,8 +10,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
     if (timeRemaining <= 0) {
         clearInterval(timer.current);
-        setTimeRemaining(targetTime * 1000);
         dialog.current.open();
+    }
+
+    function handleReset() {
+        setTimeRemaining(targetTime * 1000);
     }
 
     function handleStart() {
@@ -27,7 +30,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
     return (
         <>
-            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <ResultModal
+                ref={dialog}
+                targetTime={targetTime}
+                remainingTime={timeRemaining}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{title}</h2>
                 <p className="challenge-time">
