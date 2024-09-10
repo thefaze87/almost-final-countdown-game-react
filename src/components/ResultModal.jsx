a01bbe2b8dbe1b9ebdefaaa7d547806ae8016e27
Mark Fasel <markfasel@gmail.com>
2024-09-10 12:50:51 -0400

    add on close to dialog for esc key binding

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -7,6 +7,8 @@ const ResultModal = forwardRef(function ResultModal(
     const dialog = useRef();
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -15,8 +17,9 @@ const ResultModal = forwardRef(function ResultModal(
         };
     });
     return (
-        <dialog ref={dialog} className="result-modal">
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>
                 The target time was <strong>{targetTime} seconds</strong>.
             </p>
